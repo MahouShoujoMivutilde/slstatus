2202e63f4e3ec623260419f21efda5996b97f675
MahouShoujoMivutilde <MahouShoujoMivutilde@users.noreply.github.com>
2019-09-03 19:13:30 +0300

    simple status

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 100;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -63,5 +63,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ ram_used, " %s | ",  NULL},
+	{ keymap,   " %s | ",  NULL},
+	{ datetime, " %s ",  "%H:%M (%A) %d %b %Y" },
 };
